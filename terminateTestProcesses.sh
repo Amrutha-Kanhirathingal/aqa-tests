9c93a45377f69903c739b964759a98f3f2226d4b
Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>
2025-02-11 16:10:47 +0530

    added debug commands

--- terminateTestProcesses.sh
+++ terminateTestProcesses.sh
@@ -21,24 +21,30 @@ if [ "$OS" = "Windows_NT" ]; then
               not CommandLine like '%agent.jar%' and \
               not CommandLine like '%grep%'"
 
-  count=`powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\\\r"`
-  
+  count=$(powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\r")
+
   if [ $count -gt 0 ]; then
-      echo Windows rogue processes detected, attempting to stop them..
-      powershell -c "Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}"
-      powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}).Terminate()"
-      echo Sleeping for 10 seconds...
-      sleep 10
-      count=`powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\\\r"`
-      if [ $count -gt 0 ]; then
-        echo "Cleanup failed, ${count} processes still remain..."
-        exit 127
-      fi 
-      echo "Processes stopped successfully"
+    echo Windows rogue processes detected, attempting to stop them..
+    echo "ignore_str=${ignore_str} match_str=${match_str}"
+    powershell -c "Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}"
+    powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}).Terminate()"
+    #check after termination again the process are running or not
+    echo after process termination
+    powershell -c "Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}"
+    echo forceful termination
+    powershell -c "& { Get-Process | Where-Object { ${ignore_str} -and ${match_str} } | Stop-Process -Force }"
+    echo Sleeping for 10 seconds...
+    sleep 10
+    count=$(powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\r")
+    if [ $count -gt 0 ]; then
+      echo "Cleanup failed, ${count} processes still remain..."
+      exit 127
+    fi
+    echo "Processes stopped successfully"
   else
-      echo Woohoo - no rogue processes detected!
+    echo Woohoo - no rogue processes detected!
   fi
-elif [ `uname` = "Darwin" ]; then
+elif [ $(uname) = "Darwin" ]; then
   echo "Mac type machine.. do not terminate any remaining processes as Orka mac VMs are not process isolated, see: https://github.com/adoptium/aqa-tests/issues/4964"
 else
   echo "Unix type machine.."
@@ -49,44 +55,43 @@ else
   # Ignore Jenkins agent and grep cmd
   ignore_str="remoting.jar|agent.jar|grep"
 
-  if [ `uname` = "SunOS" ]; then
-      PSCOMMAND="/usr/ucb/ps -uxww"
+  if [ $(uname) = "SunOS" ]; then
+    PSCOMMAND="/usr/ucb/ps -uxww"
   else
-      PSCOMMAND="ps -fu $1"
+    PSCOMMAND="ps -fu $1"
   fi
 
   LINUX_DOCKER_FILTER=""
-  if [ `uname` = "Linux" ]; then
-      if egrep "\/docker\/" /proc/1/cgroup >nul; then
-          echo Running in a Linux docker container
-      else
-          echo Running on a Linux host
-          # Filter any possible docker container processes by cgroup containing "/docker"
-          PSCOMMAND="ps -o cgroup,pid,state,tname,time,command -u $1"
-          LINUX_DOCKER_FILTER="| egrep -v '^[^[:space:]]+\/docker'"
-      fi
+  if [ $(uname) = "Linux" ]; then
+    if egrep "\/docker\/" /proc/1/cgroup >nul; then
+      echo Running in a Linux docker container
+    else
+      echo Running on a Linux host
+      # Filter any possible docker container processes by cgroup containing "/docker"
+      PSCOMMAND="ps -o cgroup,pid,state,tname,time,command -u $1"
+      LINUX_DOCKER_FILTER="| egrep -v '^[^[:space:]]+\/docker'"
+    fi
   fi
 
   if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-      echo Boooo - there are rogue processes kicking about
-      echo Issuing a kill to all processes shown above
-      eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill"
-      echo Sleeping for 10 seconds...
-      sleep 10
+    echo Boooo - there are rogue processes kicking about
+    echo Issuing a kill to all processes shown above
+    eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill"
+    echo Sleeping for 10 seconds...
+    sleep 10
+    if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
+      echo Still processes left going to remove those with kill -KILL ...
+      eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill -KILL"
+      echo DONE - One final check ...
       if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-        echo Still processes left going to remove those with kill -KILL ...
-        eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill -KILL"
-        echo DONE - One final check ...
-        if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-          echo "Cleanup failed, processes still remain..."
-          exit 127
-        fi
+        echo "Cleanup failed, processes still remain..."
+        exit 127
       fi
-      echo "Processes stopped successfully"
+    fi
+    echo "Processes stopped successfully"
   else
-      echo Woohoo - no rogue processes detected!
+    echo Woohoo - no rogue processes detected!
   fi
 fi
 
 exit 0
-
