a418795ec21c2e08371807f02cd1a27c3e80a13e
Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>
2025-02-14 00:03:18 +0530

    latest changes

--- terminateTestProcesses.sh
+++ terminateTestProcesses.sh
@@ -7,58 +7,43 @@ if [ "$OS" = "Windows_NT" ]; then
   echo "Windows machine, using powershell queries..."
 
   # Match anything that is most likely a test job or process related
-  match_str=@"(CommandLine like '%java%jck%' or
-             CommandLine like '%jdkbinary%java%' or
-             CommandLine like '%java%javatest%' or
-             CommandLine like '%java%-Xfuture%' or
-             CommandLine like '%rmid%' or
-             CommandLine like '%rmiregistry%' or
-             CommandLine like '%tnameserv%' or
-             CommandLine like '%make.exe%')"@
+  match_str="(CommandLine LIKE '%java%jck%' OR \
+             CommandLine LIKE '%jdkbinary%java%' OR \
+             CommandLine LIKE '%java%javatest%' OR \
+             CommandLine LIKE '%java%-Xfuture%' OR \
+             CommandLine LIKE '%rmid%' OR \
+             CommandLine LIKE '%rmiregistry%' OR \
+             CommandLine LIKE '%tnameserv%' OR \
+             CommandLine LIKE '%make.exe%')"
 
   # Ignore Jenkins agent and grep cmd
-  ignore_str=@"not CommandLine like '%remoting.jar%' and \
-              not CommandLine like '%agent.jar%' and \
-              not CommandLine like '%grep%'"@
+  ignore_str="(NOT CommandLine LIKE '%remoting.jar%' and \
+              NOT CommandLine LIKE '%agent.jar%' and \
+              NOT CommandLine LIKE '%grep%')"
 
-  # count=`powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\\\r"`
-  # powershell -c "Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}"
-  # powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}).Terminate()"
-    powershell -c "Get-WmiObject Win32_Process | Where-Object { \$_.CommandLine -match '$match_str' -and \$_.CommandLine -notmatch '$ignore_str' }"
-    count=$(powershell -c "(Get-WmiObject Win32_Process | Where-Object { \$_.CommandLine -match '$match_str' -and \$_.CommandLine -notmatch '$ignore_str' } | Measure-Object).Count" | tr -d '\r')
-    echo "Matching process count: $count"
-    pids=$(powershell -c "(Get-WmiObject Win32_Process | Where-Object { \$_.CommandLine -match '$match_str' -and \$_.CommandLine -notmatch '$ignore_str' }).ProcessId")
-    if [ $count -gt 0 ]; then
-    echo Windows rogue processes detected, attempting to stop them..
-    for pid in $pids; do
-      powershell -c "Stop-Process -Id $pid -Force"
-    done
-    echo Sleeping for 10 seconds...
-    sleep 10
+  filter_query="$match_str AND $ignore_str"                      
 
-    # Find the workspace path dynamically from the first matching process
-    workspace_path=$(powershell -c "\$process = Get-WmiObject Win32_Process | Where-Object { \$_.CommandLine -match '$match_str' -and \$_.CommandLine -notmatch '$ignore_str' } | Select-Object -First 1;if (\$process) { \$process.CommandLine -match '([A-Za-z]:\\\\[^ ]+)' | Out-Null; echo \$matches[1] }")
-
-    # If a workspace path is found, delete only its contents (not the folder itself)
-    if [[ -n "$workspace_path" ]]; then
-      echo "Clearing workspace: $workspace_path"
-      powershell -c "Remove-Item -Path '$workspace_path\*' -Recurse -Force"
-    else
-      echo "No workspace found for cleanup."
-    fi
-    # count=`powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}} | measure).count" | tr -d "\\\\r"`
-    count= $(powershell -c "(Get-WmiObject Win32_Process | Where-Object { \$_.CommandLine -match '$match_str' -and \$_.CommandLine -notmatch '$ignore_str' } | Measure-Object).Count" | tr -d '\r')
-    echo "Matching process after force stop count: $count"
-    
-    if [ $count -gt 0 ]; then
-      echo "Cleanup failed, ${count} processes still remain..."
-      exit 127
-    fi
-    echo "Processes stopped successfully"
+  count=$(powershell -Command "(Get-WmiObject Win32_Process -Filter \"$filter_query\" | Measure-Object).Count")
+  echo "count of process =${count}"
+  if [ $count -gt 0 ]; then
+      echo Windows rogue processes detected, attempting to stop them..
+      # powershell -c "Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}"
+      # powershell -c "(Get-WmiObject Win32_Process -Filter {${ignore_str} and ${match_str}}).Terminate()"
+      powershell -Command "Get-WmiObject Win32_Process -Filter \"$filter_query\" | Select-Object ProcessId,CommandLine"
+      powershell -Command "Get-WmiObject Win32_Process -Filter \"$filter_query\" | ForEach-Object { \$_.Terminate() }"
+      echo Sleeping for 10 seconds...
+      sleep 10
+      count=$(powershell -Command "(Get-WmiObject Win32_Process -Filter \"$filter_query\" | Measure-Object).Count")
+      echo "count of process after termination =${count}"
+      if [ $count -gt 0 ]; then
+        echo "Cleanup failed, ${count} processes still remain..."
+        exit 127
+      fi 
+      echo "Processes stopped successfully"
   else
-    echo Woohoo - no rogue processes detected!
+      echo Woohoo - no rogue processes detected!
   fi
-elif [ $(uname) = "Darwin" ]; then
+elif [ `uname` = "Darwin" ]; then
   echo "Mac type machine.. do not terminate any remaining processes as Orka mac VMs are not process isolated, see: https://github.com/adoptium/aqa-tests/issues/4964"
 else
   echo "Unix type machine.."
@@ -69,43 +54,44 @@ else
   # Ignore Jenkins agent and grep cmd
   ignore_str="remoting.jar|agent.jar|grep"
 
-  if [ $(uname) = "SunOS" ]; then
-    PSCOMMAND="/usr/ucb/ps -uxww"
+  if [ `uname` = "SunOS" ]; then
+      PSCOMMAND="/usr/ucb/ps -uxww"
   else
-    PSCOMMAND="ps -fu $1"
+      PSCOMMAND="ps -fu $1"
   fi
 
   LINUX_DOCKER_FILTER=""
-  if [ $(uname) = "Linux" ]; then
-    if egrep "\/docker\/" /proc/1/cgroup >nul; then
-      echo Running in a Linux docker container
-    else
-      echo Running on a Linux host
-      # Filter any possible docker container processes by cgroup containing "/docker"
-      PSCOMMAND="ps -o cgroup,pid,state,tname,time,command -u $1"
-      LINUX_DOCKER_FILTER="| egrep -v '^[^[:space:]]+\/docker'"
-    fi
+  if [ `uname` = "Linux" ]; then
+      if egrep "\/docker\/" /proc/1/cgroup >nul; then
+          echo Running in a Linux docker container
+      else
+          echo Running on a Linux host
+          # Filter any possible docker container processes by cgroup containing "/docker"
+          PSCOMMAND="ps -o cgroup,pid,state,tname,time,command -u $1"
+          LINUX_DOCKER_FILTER="| egrep -v '^[^[:space:]]+\/docker'"
+      fi
   fi
 
   if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-    echo Boooo - there are rogue processes kicking about
-    echo Issuing a kill to all processes shown above
-    eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill"
-    echo Sleeping for 10 seconds...
-    sleep 10
-    if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-      echo Still processes left going to remove those with kill -KILL ...
-      eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill -KILL"
-      echo DONE - One final check ...
+      echo Boooo - there are rogue processes kicking about
+      echo Issuing a kill to all processes shown above
+      eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill"
+      echo Sleeping for 10 seconds...
+      sleep 10
       if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
-        echo "Cleanup failed, processes still remain..."
-        exit 127
+        echo Still processes left going to remove those with kill -KILL ...
+        eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER} | awk '{print \$2}' | xargs -n1 kill -KILL"
+        echo DONE - One final check ...
+        if eval "$PSCOMMAND | egrep '${match_str}' | egrep -v '${ignore_str}' ${LINUX_DOCKER_FILTER}"; then
+          echo "Cleanup failed, processes still remain..."
+          exit 127
+        fi
       fi
-    fi
-    echo "Processes stopped successfully"
+      echo "Processes stopped successfully"
   else
-    echo Woohoo - no rogue processes detected!
+      echo Woohoo - no rogue processes detected!
   fi
 fi
 
 exit 0
+
