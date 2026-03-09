d15b73bb30e2df996d9e0bd82ba493a2d2b3b1e0
Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>
2026-03-09 14:40:53 +0530

    Test

--- buildenv/jenkins/cleanupTestLib.groovy
+++ buildenv/jenkins/cleanupTestLib.groovy
@@ -3,7 +3,7 @@ def dependencyDir = params.DEPENDENCY_DIR ?: 'externalDependency'
 def platformMap = [
     'amac' : '(ci.role.test||ci.role.test.fips)&&hw.arch.aarch64&&(sw.os.osx||sw.os.mac)',
     'xmac' : '(ci.role.test||ci.role.test.fips)&&hw.arch.x86&&sw.os.mac',
-    'xlinux' : 'rhel9x86-svl-rt4-1.fyre.ibm.com',
+    'xlinux' : 'rhel9x86-rtp-rt2-1.fyre.ibm.com||sles15x86-svl-rt2-1.fyre.ibm.com||tj-sles16x86-rtp-test-2kari.dev.fyre.ibm.com',
     'plinux' : '(ci.role.test||ci.role.test.fips)&&hw.arch.ppc64le&&sw.os.linux',
     'zlinux' : '(ci.role.test||ci.role.test.fips)&&hw.arch.s390x&&sw.os.linux',
     'alinux' : '(ci.role.test||ci.role.test.fips)&&hw.arch.aarch64&&sw.os.linux',
